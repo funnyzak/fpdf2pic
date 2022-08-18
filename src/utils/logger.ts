cf455be83c18f9232fd508fc348d8aca56787c88
leon <silenceace@gmail.com>
2022-08-18 19:19:21 +0800

    logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,8 +4,8 @@
 
 import chalk from 'chalk';
 
-const http = (...message: string[]) =>
-  console.info(chalk.bgBlue.bold(' HTTP '), ...message);
+const ok = (...message: string[]) =>
+  console.info(chalk.bgBlue.bold(' OK '), ...message);
 const info = (...message: string[]) =>
   console.info(chalk.bgMagenta.bold(' INFO '), ...message);
 const warn = (...message: string[]) =>
@@ -14,4 +14,4 @@ const error = (...message: string[]) =>
   console.error(chalk.bgRed.bold(' ERROR '), ...message);
 const log = console.log;
 
-export const logger = { http, info, warn, error, log };
+export const logger = { ok, info, warn, error, log };
